3f999a97dcc00ccfe0026a1a61583580116527d2
tonyndungu <anthonymsalami@gmail.com>
2022-12-08 09:52:15 -0800

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,11 +15,11 @@ long long int lli;
 float f;
 
 
-printf("Size of a char: %lu bytes(s)\n", sizeof(c));
-printf("Size of an int: %lu byte(s)\n", sizeof(i));
-printf("Size of a long int: %lu byte(s)\n", sizeof(li));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
-printf("Size of a float: %lu byte(s)\n", sizeof(f));
+printf("Size of a char: %ld bytes(s)\n", sizeof(c));
+printf("Size of an int: %ld byte(s)\n", sizeof(i));
+printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+printf("Size of a float: %ld byte(s)\n", sizeof(f));
 
 return (0);
 

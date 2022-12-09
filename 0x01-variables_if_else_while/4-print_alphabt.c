93189c48d96aa78eb81a7b72ace1783e67c170dd
tonyndungu <anthonymsalami@gmail.com>
2022-12-09 08:24:08 -0800

    commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,11 +3,11 @@
 int main(void)
 {
 char ch;
-for (ch ='a'; ch <= 'z' ; ch++ )
+for (ch = 'a'; ch <= 'z' ; ch++)
 {
 if (ch != 'q' && ch != 'e')
 putchar(ch);
 }
 putchar('\n');
-return(0);
+return (0);
 }  

beaf5ca3af2915fbc8f9d331d335080d6ca35a06
tonyndungu <anthonymsalami@gmail.com>
2022-12-13 11:28:58 -0800

    commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,4 @@
-#include "main.c"
+#include "main.h"
 
 /**
  * _islower - function that checks for lowercase character.
@@ -19,4 +19,18 @@ int _islower(int c)
 		return (0);
 
 
+}
+
+int main(void)
+{
+    int r;
+
+    r = _islower('H');
+    _putchar(r + '0');
+    r = _islower('o');
+    _putchar(r + '0');
+    r = _islower(108);
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
 }

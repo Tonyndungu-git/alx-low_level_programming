3ae5d04b0e352875c80d8bef101d0ec51d4097e9
tonyndungu <anthonymsalami@gmail.com>
2022-12-13 07:08:05 -0800

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,12 @@
 #include <unistd.h>
 #define MAXSTRING 80
-
+#include "main.h"
 /**
  * main - check the code
  * Description : Its prints a string using _putchar
  * Return: Always 0.
  */
 
-/**
- * main - check the code
- * writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 int main(void)
 {
 	char message[] = "_putchar";
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function that prints the alphabet, lowercase
+ * You can only use _putchar twice in your code
+ * Return: nothing.
+ */
+
+void print_alphabet(void)
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+
+	}
+
+	_putchar('\n');
+
+}

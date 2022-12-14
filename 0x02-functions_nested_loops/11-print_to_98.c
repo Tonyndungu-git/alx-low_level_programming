78a2c77033fb2994b3217d17dfca7ec4cf3cb746
tonyndungu <anthonymsalami@gmail.com>
2022-12-14 08:42:59 -0800

    commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - function that prints every no tho 98
+ * @n: integer from which the counting starts.
+ * Return: nothing.
+ */
+
+
+void print_to_98(int n)
+{
+	int l;
+
+	if (n < 98)
+	{
+		for (l = n; l <= 98; l++)
+		{
+
+			printf("%d, ", l);
+
+		}
+	}
+
+	else
+	{
+		for (l = n; l >= 98; l--)
+		{
+			printf("%d, ", l);
+
+
+		}
+
+
+	}
+	printf("\n");
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,3 +9,4 @@ int print_sign(int n);
 void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
+void print_to_98(int n);

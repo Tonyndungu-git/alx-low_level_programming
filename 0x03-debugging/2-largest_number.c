b11998e049f4acf19c03ba6bc2920a3dd637e73a
tonyndungu <anthonymsalami@gmail.com>
2022-12-15 01:15:52 -0800

    commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a >= b && b > c)
-    {
-        largest = a;
-    }
-    if (b >= a && a > c)
-    {
-        largest = b;
-    }
-    if (c > a && c > b)
-    {
-        largest = c;
-    }
+	if (a >= b && b > c)
+	{
+		largest = a;
+	}
+	if (b >= a && a > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
 
-    return (largest);
+	return (largest);
 }

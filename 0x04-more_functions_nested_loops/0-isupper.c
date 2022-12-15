5a5133a072a1109252beaeb504ca5e0586a0cfb3
tonyndungu <anthonymsalami@gmail.com>
2022-12-15 02:07:32 -0800

    commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - Prints 1 if a char is upper case and 0 if not.
+ * @c: charcter
+ * Return: 0 or 1.
+ */
+
+int _isupper(int c)
+{
+
+	if (c >= 'A' && c <)
+	{
+		return (1);
+
+	}
+	return (0);
+}

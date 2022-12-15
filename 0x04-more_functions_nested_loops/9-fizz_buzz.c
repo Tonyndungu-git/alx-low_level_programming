7c1d30f688d6e3a023c6d2262146d0d96f55d6b6
tonyndungu <anthonymsalami@gmail.com>
2022-12-15 12:29:35 -0800

    commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,16 +12,16 @@ int  main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 15) == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
-		else if ((i % 5) == 0)
-			printf("Buzz ");
-		else if ((i % 3) == 0)
+		else if (i  % 3 == 0 && i  % 5 != 0)
 			printf("Fizz ");
+		else if (i == 100)
+			printf("Buzz");
+		else if (i % 5 == 0 && i % 3 != 0)
+			printf("Buzz ");
 		else
 			printf("%d ", i);
-
-	}
-	printf("\n");
+	}	printf("\n");
 	return (0);
 }

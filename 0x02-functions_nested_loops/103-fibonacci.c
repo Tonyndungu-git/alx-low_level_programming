cd4f39227aa55ce5c9312b3b7cba3d881c2d3cca
tonyndungu <anthonymsalami@gmail.com>
2022-12-17 07:19:44 -0800

    commit

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,18 +8,18 @@
 int main(void)
 {
 
-	long n1 =1 ;
+	long n1 = 1;
 	long n2 = 2;
 	long n3;
 	long n4;
 	int i;
 
-	for(i = 2 ; i < 50; i++)
+	for (i = 2; i < 50; i++)
 	{
 		n3 = n1 + n2;
 		if (n3 < 4000000)
 		{
-			if ( n3 % 2 == 0)
+			if (n3 % 2 == 0)
 			{
 				n4 += n3;
 			}

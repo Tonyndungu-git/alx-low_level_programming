c81a625c2fc6447fd9c19887638bdc52433419ff
tonyndungu <anthonymsalami@gmail.com>
2022-12-19 03:18:24 -0800

    commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * reset_to_98 - resets the value of a given address
+ * @n: input argument is an integer pointer.
+ * Return: void
+*/
+
 void reset_to_98(int *n)
 {
 	*n = 98;

de5a1a0ace64be277be64ef0beb7599c9d9e0464
tonyndungu <anthonymsalami@gmail.com>
2022-12-19 12:23:42 -0800

    commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: input is a string.
+ *Return: void
+ */
+
+void puts_half(char *str)
+{
+
+	int i = 0;
+	int n;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	if (i % 2 != 0)
+		n = (i - 1) / 2;
+	else
+		n = i / 2;
+	while (str[n] != '\0')
+	{
+		_putchar(*(str + n));
+		n++;
+	}
+	_putchar('\n');
+}

200525b5314e819927a985bd43327e89994d5ca5
tonyndungu <anthonymsalami@gmail.com>
2022-12-20 12:14:24 -0800

    commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * _strcpy - copies a string to a different address.
+ * @dest: destination pointer
+ * @src: original address pointer
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+
+
+	int i;
+
+	i = 0;
+	while (src[i] != '0')
+	{
+
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i)  = '\0';
+
+	return (dest);
+}

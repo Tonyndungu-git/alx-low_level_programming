4804d03ebf1fedf08f7dac2412cfb9bf5dd67465
tonyndungu <anthonymsalami@gmail.com>
2022-12-20 06:17:41 -0800

    commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,7 +9,7 @@ void puts_half(char *str)
 {
 
 	int i = 0;
-	int n;
+	int n = 0;
 
 	while (str[i] != '\0')
 	{
@@ -17,7 +17,7 @@ void puts_half(char *str)
 	}
 	n = i / 2;
 	if (i % 2 == 1)
-		n =+ 1;
+		n ++;
 	while (str[n] != '\0')
 	{
 		_putchar(*(str + n));

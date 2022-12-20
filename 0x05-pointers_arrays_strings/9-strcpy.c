5e85877d523728fe7857444e4d173cf959f8a837
tonyndungu <anthonymsalami@gmail.com>
2022-12-20 12:16:33 -0800

    commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,18 +8,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-
-
 	int i;
 
-	i = 0;
-	while (src[i] != '0')
+	for (i = 0; src[i] != '\0'; i++)
 	{
-
 		*(dest + i) = *(src + i);
-		i++;
 	}
-	*(dest + i)  = '\0';
-
+	*(dest + i) = '\0';
 	return (dest);
 }

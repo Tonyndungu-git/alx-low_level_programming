ced10837d5995c8d52e4378b4daf06386778ed49
tony ngundungu <anthonymsalami@gmail.com>
2022-12-21 20:33:50 +0300

    commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes a string to uppercase.
+ * @ch: pointer of ch.
+ * Return: character.
+ */
+
+char *string_toupper(char *ch)
+{
+	int i;
+
+	for (i = 0; *(ch + i) != '\0'; i++)
+	{
+
+
+
+		if (*(ch + i) >= 97 && *(ch + i) <= 122)
+		{
+			*(ch + i) = *(ch + i) - ' ';
+
+		}
+		return (ch);
+
+
+	}
+}

85e799af23bf1f1307b1cc823feb2ac6b6e32436
tony ngundungu <anthonymsalami@gmail.com>
2022-12-21 23:02:55 +0300

    commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,10 @@
 #include "main.h"
-#include <stdio.h>
-
+/**
+ * reverse_array - reverses an array
+ * @a: pointer to a.
+ * @n: ponter to b.
+ * Result: void.
+ */
 void reverse_array(int *a, int n)
 {
 	int i = 0;
@@ -9,10 +13,10 @@ void reverse_array(int *a, int n)
 	int *k;
 	int  m = 0;
 
-	l =a;
+	l = a;
 	k = a;
 	n--;
-	
+
 	while (i < n)
 	{
 		i++;
@@ -21,39 +25,10 @@ void reverse_array(int *a, int n)
 	while (j <= i)
 	{
 		m = *(l + j);
-	        *(l + j) = *(k + i);
+		*(l + j) = *(k + i);
 		*(k + i) = m;
-		
 
 		j++;
 		i--;
 	}
-}
-
-
-void print_array(int *a, int n)
-{
-    int i;
-
-    i = 0;
-    while (i < n)
-    {
-        if (i != 0)
-        {
-            printf(", ");
-        }
-        printf("%d", a[i]);
-        i++;
-    }
-    printf("\n");
-}
-
-int main(void)
-{
-    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
-
-    print_array(a, sizeof(a) / sizeof(int));
-    reverse_array(a, sizeof(a) / sizeof(int));
-    print_array(a, sizeof(a) / sizeof(int));
-    return (0);
 }

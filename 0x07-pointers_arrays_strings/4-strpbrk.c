76530aa26d5916c1fbebd536ae01f140c971dec7
tony ngundungu <anthonymsalami@gmail.com>
2023-01-03 18:10:52 +0300

    commit

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i =0;
+	int j =0;
+
+	for (i = 0; s[i] != '\0'; i++) {
+		for (j = 0; accept[j] != '\0'; j++) {
+			if (s[i] == accept[j]) {
+				return (s + i);
+			}
+		}
+	}
+	return NULL;
+}

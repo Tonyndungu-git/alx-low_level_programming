fb99f6e68ce633dc9ca7a23aa5b0fe5647ceae51
tony ngundungu <anthonymsalami@gmail.com>
2023-01-04 12:42:38 +0300

    commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,33 +10,24 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i = 0;
-	unsigned int j = 0;
-	int c;
 	unsigned int cont = 0;
+	int j = 0;
+	int flag;
 
 	while (*s != '\0')
 	{
-		c = 0;
-
+		flag = 0;
 		while (*(accept + j) != '\0')
 		{
-
 			if (*s == *(accept + j))
-				c = 1;
-			j++;
+				flag = 1;
+			j += 1;
 		}
-		j= 0;
-
-		if (c  == 0)
-		{
+		j = 0;
+		if (flag == 0)
 			break;
-		}
 		cont += 1;
 		s++;
-
-
 	}
 	return (cont);
-
 }

4379cd54763244aeafe59a9e2daf2633750d51fd
tony ngundungu <anthonymsalami@gmail.com>
2023-01-09 13:44:42 +0300

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program.
+ * @argc: count of number of inputs.
+ * @argv: an array of the inputs.
+ * Return: integer.
+ */
+
+int main(int argc, char *argv[])
+{
+
+
+	printf("%s\n", argv[0]);
+	return (0);
+
+}

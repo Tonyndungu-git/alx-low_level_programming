ed605826ebc4535b5ea1fe868a107f846b9d1a28
tony ngundungu <anthonymsalami@gmail.com>
2023-01-11 13:46:15 +0300

    commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,6 +13,7 @@
 int **alloc_grid(int width, int height)
 {
 	int i;
+	int **n;
 
 	if (width <= 0 || height <= 0)
 	{
@@ -20,7 +21,7 @@ int **alloc_grid(int width, int height)
 
 	}
 
-	int **n = malloc(width * sizeof(int));
+	n = malloc(width * sizeof(int *));
 
 	i = 0;
 

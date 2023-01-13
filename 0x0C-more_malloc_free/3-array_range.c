7527a11803fc7246b28fb481f9b7e2dbd6adff61
tony ngundungu <anthonymsalami@gmail.com>
2023-01-13 20:11:30 +0300

    com mit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,7 +13,7 @@ int *array_range(int min, int max)
 	int i;
 	int *arr;
 
-	arr = malloc((max - min + 1) * sizeof(int *));
+	arr = malloc((max - min + 1) * sizeof(int));
 
 	if (min > max)
 		return (NULL);

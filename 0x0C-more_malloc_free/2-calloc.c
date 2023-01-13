de47a2088d79946ca6c4fa997222763e59cc7070
tony ngundungu <anthonymsalami@gmail.com>
2023-01-13 18:46:05 +0300

    commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,6 +15,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	void *ptr;
 	unsigned int total_size = nmemb * size;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 	ptr = malloc(total_size);
 	if (ptr == NULL)
 		return (NULL);

430d2d0d13d207627e2b8edebae06bd818195ab5
tony ngundungu <anthonymsalami@gmail.com>
2023-01-14 14:59:32 +0300

    commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
  * malloc_checked - function that allocates memory using malloc.
  * @b: unsigned int input b.
  * Return: a void pointer.
  */
+
 void *malloc_checked(unsigned int b)
 {
 
---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,62 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * mult - function that multiplies two numbers.
+ * @num1: int 1.
+ * @num2: int 2.
+ * Return: an int.
+ */
+
+long long int mult(long long int num1, long long int num2)
+{
+	return (num1 * num2);
+}
+
+/**
+ * main - function that multiplies two numbers.
+ * @argc: int of the number of arguments.
+ * @argv: array pointer of the arguments.
+ * Return: an int.
+ */
+
+int main(int argc, char *argv[])
+{
+	long long int i, j, valid = 1;
+	long long int num1, num2, m;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	i = 1;
+	while (i <= 2)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				valid = 0;
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	if (!valid)
+	{
+	    printf("Error\n");
+	    exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	m = mult(num1, num2);
+	printf("%lld\n", m);
+	return (0);
+}

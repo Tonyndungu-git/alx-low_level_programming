e16582a8c0401db3f9d8737241246d72e96fb5d6
tony ngundungu <anthonymsalami@gmail.com>
2023-01-17 20:42:54 +0300

    commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,13 +14,15 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
-	printf("Name: %s \n", d->name);
+	else
+		printf("Name: %s \n", d->name);
 
 	printf("Age: %f \n", d->age);
 
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
-	printf("Owner: %s \n", d->owner);
+	else
+		printf("Owner: %s \n", d->owner);
 
 
 

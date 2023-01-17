83201fab8db0a43481dfc31ce03a89f05010ae07
tony ngundungu <anthonymsalami@gmail.com>
2023-01-17 21:42:44 +0300

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,25 +15,11 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
-/**
- * struct dog dot_t- stores the details of a dog.
- * @name: First member
- * @owner: Second member.
- * @age: Third member
- 
-
-typedef struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-}dog_t;
-*/
 
 #endif

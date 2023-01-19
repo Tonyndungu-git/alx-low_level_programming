d37cb4d485842e0a6c04a7e92d05f0654f0e95c9
tony ngundungu <anthonymsalami@gmail.com>
2023-01-19 23:21:50 +0300

    commit

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -13,12 +13,14 @@ typedef struct op
     int (*f)(int a, int b);
 } op_t;
 
+#endif
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
 int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-
-
 #endif

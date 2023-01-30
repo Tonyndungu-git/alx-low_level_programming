17643446174c7f2f19dfe5f9f4cac0e8d675a047
tony ngundungu <anthonymsalami@gmail.com>
2023-01-30 21:50:31 +0300

    function to free up a linked list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,6 +1,13 @@
 #include "lists.h"
 #include <stddef.h>
 #include <stdlib.h>
+
+/**
+ * free_list - frees space from the list.
+ * @head: pointer to the head of the list
+ * Return: void.
+ */
+
 void free_list(list_t *head)
 {
 	list_t *tmp;
@@ -13,30 +20,4 @@ void free_list(list_t *head)
 		free(tmp);
 	}
 
-}
-
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    list_t *head;
-
-    head = NULL;
-    add_node_end(&head, "Bob");
-    add_node_end(&head, "&");
-    add_node_end(&head, "Kris");
-    add_node_end(&head, "love");
-    add_node_end(&head, "asm");
-    print_list(head);
-    free_list(head);
-    head = NULL;
-    return (0);
 }

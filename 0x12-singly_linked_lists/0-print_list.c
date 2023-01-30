9802e338ab58a91a61e47085831a3bc90a298a26
tony ngundungu <anthonymsalami@gmail.com>
2023-01-30 17:27:54 +0300

    commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,23 +11,22 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t cont = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] (nill)\n");
+			printf("[0] (nil)\n");
 			h = h->next;
-			count++;
+			cont++;
 		}
 		else
 		{
 			printf("[%u] %s\n", h->len, h->str);
 			h = h->next;
-			count++;
+			cont++;
 		}
 	}
-	return (count);
+	return (cont);
 }
-

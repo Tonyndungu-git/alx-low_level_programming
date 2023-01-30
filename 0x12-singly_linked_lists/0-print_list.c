75ac9b97819215ba1b64530eb14168a036ec1d6e
tony ngundungu <anthonymsalami@gmail.com>
2023-01-30 16:36:17 +0300

    made sure that betty was working

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,56 +2,23 @@
 #include <stdio.h>
 #include <stddef.h>
 
+
+/**
+ * print_list - prints contents of a linked list.
+ * @h: pointer to the head of the list.
+ * Return: number of nodes iin the list.
+ */
+
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 	const list_t *current = h;
+
 	while (current)
 	{
 		printf("[%u] %s\n", current->len, current->str ? current->str : "(nil)");
 		current = current->next;
 		count++;
 	}
-	return count;
-}
-
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    list_t *head;
-    list_t *new;
-    list_t hello = {"World", 5, NULL};
-    size_t n;
-
-    head = &hello;
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->str = strdup("Hello");
-    new->len = 5;
-    new->next = head;
-    head = new;
-    n = print_list(head);
-    printf("-> %lu elements\n", n);
-
-    printf("\n");
-    free(new->str);
-    new->str = NULL;
-    n = print_list(head);
-    printf("-> %lu elements\n", n);
-
-    free(new);
-    return (0);
+	return (count);
 }

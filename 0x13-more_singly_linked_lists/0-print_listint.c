312c7695130bb1789509564e1e1405cc1a7912f0
tony ngundungu <anthonymsalami@gmail.com>
2023-02-03 17:33:12 +0300

    commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * print_listint - function that prints elements in a linked list.
+ * @h: a pointer to a struct.
+ * Return: size of linked list.
+ */
 
 size_t print_listint(const listint_t *h)
 {
@@ -15,39 +20,10 @@ size_t print_listint(const listint_t *h)
 		}
 		else
 		{
-		printf("%d\n", h->n);
-		h = h->next;
-		count++;
+			printf("%d\n", h->n);
+			h = h->next;
+			count++;
 		}
 	}
-	h->next;
 	return (count);
-}
-
-/**
- * main - check the code
- * 
- * Return: Always 0.
- */
-int main(void)
-{
-    listint_t *head;
-    listint_t *new;
-    listint_t hello = {8, NULL};
-    size_t n;
-
-    head = &hello;
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->n = 9;
-    new->next = head;
-    head = new;
-    n = print_listint(head);
-    printf("-> %lu elements\n", n);
-    free(new);
-    return (0);
 }

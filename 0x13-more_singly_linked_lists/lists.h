4f2a8c29b77e81ea026a5680e1553bf976ae3f9c
tony ngundungu <anthonymsalami@gmail.com>
2023-02-04 19:57:50 +0300

    commit

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -28,5 +28,6 @@ listint_t *reverse_listint(listint_t **head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 void free_listint2(listint_t **head);
+int pop_listint(listint_t **head)
 
 #endif

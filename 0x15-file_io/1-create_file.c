f2566a6f67a010992e95c44ea62adcdb993e9f07
tony ngundungu <anthonymsalami@gmail.com>
2023-02-08 20:22:15 +0300

    commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,6 +32,6 @@ int create_file(const char *filename, char *text_content)
 	len = strlen(text_content);
 	fd_write = write(fd, text_content, len);
 
-	close(fd);
+	close(fd_write);
 	return (1);
 }

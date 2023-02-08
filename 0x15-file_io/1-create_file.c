d6b83309c566abf0919fe24ad336938fe78b7e53
tony ngundungu <anthonymsalami@gmail.com>
2023-02-08 19:29:51 +0300

    commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/**
+ * create_file - funtion that creates a file.
+ * @filename:pointer to the name of the new file.
+ * @text_content:content to be written to the file.
+ * Return: an int.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len, fd_write;
+
+	if (filename == NULL || text_content == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0644);
+
+
+
+	if (fd == -1)
+		return (-1);
+
+	len = strlen(text_content);
+	fd_write = write(fd, text_content, len);
+
+	close(fd);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,8 +2,7 @@
 #define _MAIN_H
 
 int _putchar(char c);
-
 ssize_t read_textfile(const char *filename, size_t letters);
-
+int create_file(const char *filename, char *text_content);
 
 #endif

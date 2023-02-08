1b4b0cdfc5e438c503f59b0bec73c590ea5c9676
tony ngundungu <anthonymsalami@gmail.com>
2023-02-08 22:55:08 +0300

    commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,29 +1,48 @@
-#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
 /**
- * create_file - creates a file and fills it with text
- * @filename: name of the file to create
- * @text_content: text to write in the file
+ * _strlen - function that returns the length of a string.
  *
+ * @s: pointer to an string
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i += 1;
+	}
+	return (i);
+}
+
+/**
+ * create_file - function that create a file
+ * @filename: name of file to be created
+ * @text_content: NULL terminated string to write to the file
  * Return: 1 on success, -1 on failure
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int fd, x, y = 0;
+	int fd, fd_write;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fd < 0)
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
-	if (text_content)
-	{
-		while (text_content[y])
-			y++;
-		x = write(fd, text_content, y);
-		if (x != y)
-			return (-1);
-	}
+	if (text_content == NULL)
+		return (1);
+	fd_write = write(fd, text_content, _strlen(text_content));
 	close(fd);
+	if (fd_write == -1)
+		return (-1);
 	return (1);
 }

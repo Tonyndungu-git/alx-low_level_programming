d5df252b429e5465dd998ebdee765dabcac931b0
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-03-16 15:21:36 +0300

    commit

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,23 +9,18 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node;
+	dlistint_t *new_node;
 
-    if (head == NULL)
-        return (NULL);
-
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return (NULL);
-
-    new_node->n = n;
-    new_node->prev = NULL;
-
-    if (*head != NULL)
-        (*head)->prev = new_node;
-
-    new_node->next = *head;
-    *head = new_node;
-
-    return (new_node);
+	if (head == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = new_node;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }

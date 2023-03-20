677d0644cc2b64dffde24d24903496108e391b5d
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-03-20 20:00:35 +0300

    commit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/libfix.so https://github.com/Nachop51/holbertonschool-low_level_programming/blob/main/0x18-dynamic_libraries/libfix.so?raw=true
+wget -O /tmp/libfix.so https://github.com/Tonyndungu-git/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libfix.so?raw=true
 export LD_PRELOAD=/tmp/libfix.so

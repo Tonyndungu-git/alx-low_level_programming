ef2fdb67ebbccbe61a14e0893a41974e1638aec0
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-07-11 21:29:41 +0300

    corrections

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,47 +1,39 @@
 #include "search_algos.h"
 #include <math.h>
 
-int binary_search(int *array, size_t size, int value)
+int jump_search(int *array, size_t size, int value)
 {
-    if (array == NULL)
-    {
-        return -1;
-    }
+	int left = 0;
+	int right = size - 1;
+	int mid;
 
-    int left = 0;
-    int right = size - 1;
+	if (array == NULL)
+	{
+		return -1;
+	}
 
-    while (left <= right)
-    {
-        int mid = left + (right - left) / 2;
+	while (left <= right)
+	{
+		mid = left + (right - left) / 2;
 
-        printf("Searching in array: ");
-        for (int i = left; i <= right; i++)
-        {
-            printf("%d", array[i]);
-            if (i != right)
-            {
-                printf(", ");
-            }
-        }
-        printf("\n");
-
-        if (array[mid] == value)
-        {
-            return mid;
-        }
-        else if (array[mid] < value)
-        {
-            left = mid + 1;
-        }
-        else
-        {
-            right = mid - 1;
-        }
-    }
-
-    return -1;
+		printf("Value checked array[%d] = [%d]\n", mid, array[mid]);
+		
+		if (array[mid] == value)
+		{
+			return mid;
+		}
+		else if (array[mid] < value)
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			right = mid - 1;
+		}
+	}
+     	return -1;
 }
+
 
 int main(void)
 {

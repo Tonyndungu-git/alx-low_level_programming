e311cd875158fc02fa5e40cb01e3d016ace6e26b
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-07-12 15:28:38 +0300

    jump list earch algorithm

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,52 +1,45 @@
 #include "search_algos.h"
-listint_t *jump_list(listint_t *list, size_t size, int value) {
+#include "search_algos.h"
 
-	int jump = sqrt(size);
-	listint_t *current = list;
-
-
-	if (list == NULL || size == 0) {
-		return NULL;
-	}
-
-
-	while (current != NULL && current->n < value) {
-		current = current->next;
-
-		if (current - list >= jump) {
-			current = list + jump;
-		}
-	}
-
-	if (current == NULL) {
-		return NULL;
-	}
-
-	if (current->n == value) {
-		return current;
-	}
-
-	return NULL;
-}
-
-int main(void)
+/**
+ * jump_list - Searches for a value in a sorted list of integers
+ *             using the Jump search algorithm.
+ *
+ * @list: Pointer to the head of the list to search in.
+ * @size: Number of nodes in the list.
+ * @value: Value to search for.
+ *
+ * Return: A pointer to the first node where value is located,
+ *         or NULL if value is not present or if head is NULL.
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-    listint_t *list, *res;
-    int array[] = {
-        0, 1, 2, 3, 4, 7, 12, 15, 18, 19, 23, 53, 61, 62, 76, 99
-    };
-    size_t size = sizeof(array) / sizeof(array[0]);
+	size_t jump = sqrt(size);
+	listint_t *prev = NULL, *curr = list;
 
-    list = create_list(array, size);
-    print_list(list);
+	if (list == NULL)
+		return (NULL);
 
-    res =  jump_list(list, size, 53);
-    printf("Found %d at index: %lu\n\n", 53, res->index);
-    res =  jump_list(list, size, 2);
-    printf("Found %d at index: %lu\n\n", 2, res->index);
-    res =  jump_list(list, size, 999);
-    printf("Found %d at index: %p\n", 999, (void *) res);
+	while (curr != NULL && curr->n < value)
+	{
+		prev = curr;
+		while (curr->next != NULL && curr->index < jump)
+			curr = curr->next;
+		printf("Value checked at index [%lu] = [%d]\n", curr->index, curr->n);
+		jump += sqrt(size);
+	}
 
-    free_list(list);
-    return (EXIT_SUCCESS);
+	printf("Value found between indexes [%lu] and [%lu]\n",
+	       prev->index, curr->index);
+	jump -= sqrt(size);
+
+	while (prev != NULL && prev->index <= curr->index)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
+		if (prev->n == value)
+			return (prev);
+		prev = prev->next;
+	}
+
+	return (NULL);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - singly linked list
@@ -21,9 +22,7 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-
-
-
+listint_t *jump_list(listint_t *list, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
@@ -31,11 +30,9 @@ int exponential_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int advanced_binary_recursive(int *array, int low, int high, int value);
 int advanced_binary(int *array, size_t size, int value);
-
-
-listint_t *jump_list(listint_t *list, size_t size, int value);
 void free_list(listint_t *list);
 listint_t *create_list(int *array, size_t size);
 void print_list(const listint_t *list);
+
 
 #endif
